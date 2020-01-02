c22aae275180343e8443a454fe1a900ef237a49a
kee cha <kee_cha18@yahoo.com>
2020-01-02 10:58:38 -0600

    forgot to move git folder so i am recommiting

---
+++ Superheroes/Models/SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Superheroes.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
